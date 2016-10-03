b0955ee3d32ecba8c5fd6e98002a1d31a709bc3e
linuskuo <linus8930@gmail.com>
2016-10-02 17:54:43 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -9,3 +9,37 @@ app.config(function (localStorageServiceProvider) {
     localStorageServiceProvider
       .setPrefix('scotch-todo');
   });
+  
+   app.controller('main', function ($scope, $ionicModal, localStorageService) { //store the entities name in a variable var taskData = 'task';
+
+//initialize the tasks scope with empty array
+$scope.tasks = [];
+
+//initialize the task scope with empty object
+$scope.task = {};
+
+//configure the ionic modal before use
+$ionicModal.fromTemplateUrl('new-task-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+}).then(function (modal) {
+    $scope.newTaskModal = modal;
+});
+
+$scope.getTasks = function () {
+    //fetches task from local storage
+    ...
+}
+$scope.createTask = function () {
+    //creates a new task
+    ...
+}
+$scope.removeTask = function () {
+    //removes a task
+    ...
+}
+$scope.completeTask = function () {
+    //updates a task as completed
+    ...
+}
+})
